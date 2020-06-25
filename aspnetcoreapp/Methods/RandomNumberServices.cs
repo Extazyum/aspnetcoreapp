82a783005643fb0ac0883aa0043164672d39a571
Extazyum <nicolas.bazely@gmail.com>
2020-06-25 10:33:57 +0200

    Ajoutez des fichiers projet.

---
+++ aspnetcoreapp/Methods/RandomNumberServices.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnetcoreapp.Methods
+{
+    public class RandomNumberService
+    {
+        public  int GetNext() => new Random().Next();
+    }
+}
---
+++ aspnetcoreapp/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using aspnetcoreapp.Methods;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace aspnetcoreapp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RandomNumberService _randomNumberServices;
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger,RandomNumberService randomNumberServices)
+        {
+            _logger = logger;
+          _randomNumberServices= randomNumberServices;
+          randomNumberServices.GetNext();
+        }
+
+        public void OnGet()
+        {
+        
+        }
+    }
+}
